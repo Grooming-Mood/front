77dc474945020f2e7f7eb681c4cc1644479dcf74
kimdoha <kkhhjj3393@gmail.com>
2022-10-10 22:08:19 +0900

    Feat: Main Home 생성

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
+import React from "react";
+import {BrowserRouter, Route} from "react-router-dom";
+import Home from "./views/Home";
 import './App.css';
 
+
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    return (
+        <BrowserRouter>
+            <div className="App">
+              <div className="content">
+                  <Route path="/" component={ Home } exact />
+              </div>
+            </div>
+        </BrowserRouter>
   );
 }
 
