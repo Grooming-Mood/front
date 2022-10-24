31d102100976b4b560916738bef9817d4ce46d14
kimdoha <kkhhjj3393@gmail.com>
2022-10-24 17:32:40 +0900

    Feat: Calendar RenderCells 컴포넌트 구성
    -

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Setting from "./views/Setting";
 import Setting_Convert from "./views/Setting_Convert";
 import './App.css';
 import Feed from "./views/Feed";
+import Calendar from "./views/Calendar";
 
 
 
@@ -16,6 +17,7 @@ function App() {
               <div className="content">
                   <Route path="/" component={ Home } exact />
                   <Route path="/feed" component={ Feed } exact />
+                  <Route path="/calendar" component={ Calendar } exact />
                   <Route path="/record" component={ Record } exact /> {/*레코드 화면*/}
                   <Route path="/setting" component={ Setting } exact /> {/*회원 정보 수정*/}
                   <Route path="/setting_convert" component={ Setting_Convert } exact /> {/*회원 정보 프사, 닉네임 수정*/}
