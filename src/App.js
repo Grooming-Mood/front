8677c1a6961fd5d628c2cd6427033ba520d56097
kimdoha <kkhhjj3393@gmail.com>
2022-10-28 17:39:12 +0900

    Feat: Router 설정
    -

--- src/App.js
+++ src/App.js
@@ -8,8 +8,10 @@ import Result from "./views/Result";
 import './App.css';
 import Feed from "./views/Feed";
 import Calendar from "./views/Calendar";
-
-
+import HappyHome from "./views/HappyHome";
+import SadHome from "./views/SadHome";
+import AngryHome from "./views/AngryHome";
+import NormalHome from "./views/NormalHome";
 
 function App() {
     return (
@@ -17,7 +19,13 @@ function App() {
             <div className="App">
               <div className="content">
                   <Route path="/" component={ Home } exact />
+
                   <Route path="/feed" component={ Feed } exact />
+                  <Route path="/happy-feed" component={ HappyHome }/>
+                  <Route path="/sad-feed" component={ SadHome }/>
+                  <Route path="/angry-feed" component={ AngryHome }/>
+                  <Route path="/nomal-feed" component={ NormalHome }/>
+
                   <Route path="/calendar" component={ Calendar } exact />
                   <Route path="/record" component={ Record } exact /> {/*레코드 화면*/}
                   <Route path="/setting" component={ Setting } exact /> {/*회원 정보 수정*/}
