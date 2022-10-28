7ffbdffd4a19e665fb66f8f9fcb774480c96f469
yeonhee <ydol2@naver.com>
2022-10-28 14:51:17 +0900

    feat : result 화면 jsx 추가

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Home from "./views/Home";
 import Record from "./views/Record";
 import Setting from "./views/Setting";
 import Setting_Convert from "./views/Setting_Convert";
+import Result from "./views/Result";
 import './App.css';
 import Feed from "./views/Feed";
 import Calendar from "./views/Calendar";
@@ -21,6 +22,7 @@ function App() {
                   <Route path="/record" component={ Record } exact /> {/*레코드 화면*/}
                   <Route path="/setting" component={ Setting } exact /> {/*회원 정보 수정*/}
                   <Route path="/setting_convert" component={ Setting_Convert } exact /> {/*회원 정보 프사, 닉네임 수정*/}
+                  <Route path="/result" component={ Result } exact /> {/*회원 정보 프사, 닉네임 수정*/}
               </div>
             </div>
         </BrowserRouter>
