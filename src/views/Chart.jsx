28a807e1a457fe56b67c7c8fee759d517642a135
kimdoha <kkhhjj3393@gmail.com>
2022-11-07 14:58:09 +0900

    Feat: 감정 통계 xaxis 생성
    -

--- src/views/Chart.jsx
+++ src/views/Chart.jsx
@@ -7,29 +7,6 @@ import { RenderCells } from "../Calendar/RenderCells";
 import { CalendarHeader } from "../Calendar/CalendarHeader";
 import ApexCharts from "react-apexcharts";
 
-const options = {
-    series: [{
-        data: [400, 430, 448, 470, 540, 580, 690, 1100, 1200, 1380]
-    }],
-    chart: {
-        type: 'bar',
-        height: 350
-    },
-    plotOptions: {
-        bar: {
-            borderRadius: 4,
-            horizontal: true,
-        }
-    },
-    dataLabels: {
-        enabled: false
-    },
-    xaxis: {
-        categories: ['South Korea', 'Canada', 'United Kingdom', 'Netherlands', 'Italy', 'France', 'Japan',
-            'United States', 'China', 'Germany'
-        ],
-    }
-};
 
 function Chart(props){
     const [currentMonth, setCurrentMonth] = useState(new Date());
@@ -75,6 +52,14 @@ function Chart(props){
                                         height: 500,
                                         width: 500,
                                     },
+                                    xaxis : {
+                                        categories: [
+                                        "HAPPY",
+                                        "NORMAL",
+                                        "SAD",
+                                        "ANGRY"
+                                        ],
+                                    }
                                 }}>
                             </ApexCharts>
                         </div>
