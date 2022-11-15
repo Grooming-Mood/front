e9cf0c2e054c09a3994168e3735a1de834023253
midoi327 <midoi327@naver.com>
2022-11-15 16:21:10 +0900

    chore: Login페이지 디자인 변경

--- src/views/Login.jsx
+++ src/views/Login.jsx
@@ -5,6 +5,49 @@ import SideMenu from "./SideMenu";
 import User from "../assets/image/user/user.png";
 
 
+//로그인 컴포넌트 css
+const loginStyleTextArea = {
+    alignContent: "center",
+    borderRadius: "22px",
+    marginBottom: "35px",
+    width: "80%",
+    height: "64px",
+    boxShadow: "0 0 8px 0 rgba(0, 0, 0, 0.2), 0 0 20px 0 rgba(0, 0, 0, 0.19)"
+  };
+
+const loginStyleTextInput = {
+    marginTop: "1.2rem",
+    fontFamily: "Montserrat regular",
+    fontSize: "15px",
+    border: "none",
+    width: "80%"
+
+}
+
+const loginStyleForm = {
+    height: "50%",
+    width: "50%",
+    marginBottom: "170px"
+}
+
+const loginBtn = {
+    marginTop: "28px",
+    width: "165px",
+    height: "60px",
+    borderRadius: "30px",
+    background: "#FF99CC",
+    border: "none",
+    fontFamily: 'Montserrat',
+    fontSize: "20px",
+    color: "#FFFFFF",
+    boxShadow: "0 4px 8px 0 rgba(255, 204, 229, 0.7), 0 6px 20px 0 rgba(255, 204, 229, 0.9)",
+    cursor: "pointer"
+
+}
+
+
+
+
 function Login(props) {
 
     //아이디, 패스워드
@@ -63,24 +106,33 @@ function Login(props) {
                         </div>
                     </div>
                     <div className="setting-right">
+                        <form style={loginStyleForm}>
+                            <div style={loginStyleTextArea}>
+                                <input
+                                    style={loginStyleTextInput}
+                                    type="email"
+                                    placeholder="이메일을 입력하세요"
+                                    name="input_id"
+                                    value={inputId}
+                                    onChange={handleInputId}
+                                />
+                            </div>
+                            <div style={loginStyleTextArea}>
+                                <input
+                                    style={loginStyleTextInput}
+                                    type="password"
+                                    placeholder="비밀번호를 입력하세요"
+                                    name="input_pw"
+                                    value={inputPw}
+                                    onChange={handleInputPw}
+                                />
+                            </div>
 
-                        <input
-                            type="email"
-                            placeholder="이메일을 입력하세요"
-                            name="input_id"
-                            value={inputId}
-                            onChange={handleInputId}
-                        />
+                            <button style={loginBtn} onClick={onClickLogin}>로그인하기</button>
+                        </form>
 
-                        <input
-                            type="password"
-                            placeholder="비밀번호를 입력하세요"
-                            name="input_pw"
-                            value={inputPw}
-                            onChange={handleInputPw}
-                        />
 
-                        <button onClick={onClickLogin}>로그인하기</button>
+                        
 
 
                     </div>
