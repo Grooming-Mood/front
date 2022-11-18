cb995640a5299884bb722cbcbb0775ee9f3fdfe5
yeonhee <ydol2@naver.com>
2022-11-19 05:26:35 +0900

    feat : sad 상세보기 정렬x

--- src/views/DetailView.jsx
+++ src/views/DetailView.jsx
@@ -76,7 +76,7 @@ function DetailView() {
                     <p>당신의 감정을<br/>어루만지는 AI 일기</p>
                 </div>
                 <div className="home-content">
-                <div className="detail-container">
+                    <div className="detail-container">
                         <div className="detail-container-row">
                             <div className="detail-container-row-first">
                             <img src={NormalIcon} alt="Normal" style={{"width":"50px","hight":"50px"}}/>
@@ -106,13 +106,29 @@ function DetailView() {
         sessionStorage.removeItem(diaryId);
         console.log("AAcheckdiaryId",sessionStorage.getItem(diaryId));
         return (
-            <div className="result-happy">
-                <div className="result-happy-header"> {/*헤더*/}
-                    <Link to="/" className="result-happy-header-link">GroomingMood</Link>
+            <div className="result-sad">
+                <div className="result-sad-header"> {/*헤더*/}
+                    <Link to="/" className="result-sad-header-link">GroomingMood</Link>
                     <p>당신의 감정을<br/>어루만지는 AI 일기</p>
                 </div>
                 <div className="home-content">
-                    <div className="result-container">
+                    <div className="detail-container">
+                        <div className="detail-container-row">
+                            <div className="detail-container-row-first">
+                            <img src={SadIcon} alt="Sad" style={{"width":"50px","hight":"50px"}}/>
+                            </div>
+                            <div className="detail-container-row-first-second">
+                                {userName}
+                            </div>
+                            <div className="detail-container-row-first-thrid">
+                                {createdDate[0] +"-" + createdDate[1] + "-" + createdDate[2] + " " + createdDate[3] + ":" + createdDate[4]}
+                            </div>
+                        </div>
+                        <div className="detail-sad">
+                            <div className="detail-sad-font">
+                                {diaryContent}
+                            </div>
+                        </div>
                     </div>
                     <SideMenu></SideMenu>
                 </div>
