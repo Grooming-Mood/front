c505f8c11578fd7ee77b387c25abdd33500cf4bf
kimdoha <kkhhjj3393@gmail.com>
2022-11-18 17:22:00 +0900

    feat: pagination 수정
    -

--- src/views/Feed.jsx
+++ src/views/Feed.jsx
@@ -11,27 +11,42 @@ import axios from "axios";
 
 function Feed(props) {
     const [page, set_page] = useState(1);
+    const [postPerPage, setPostPerPage] = useState(10);
+
     const [feed, set_feed] = useState([]);
-    const [myfeed, set_myfeed] = useState([]);
     const [userId, set_userId] = useState(0);
-    const [cursor, set_cursor] = useState(1);
+    const [cursor, set_cursor] = useState("");
+    const [loading, set_loading] = useState(false);
 
     const handlePageChange = (page) => {
         set_page(page);
     };
+
     // https://chanhuiseok.github.io/posts/react-13/
     useState(() => {
+        set_loading(true);
         axios.get(`http://ec2-52-196-145-123.ap-northeast-1.compute.amazonaws.com:8080/feed-diary/all-paging?cursor=${cursor}&size=3`)
             .then((res) => {
                 const data = res.data;
                 set_feed(data.diaryList);
-                console.log(data);
-
+                console.log(cursor);
+                console.log(data.nextCursor);
                 if(data.hasNext) {
-                    set_cursor(res.data.diaryList.nextCursor);
+                    set_cursor(data.nextCursor);
+                    console.log('cursor' + cursor);
                 }
+
+                set_loading(false);
             });
     }, []);
+
+    const indexOfLast = page * postPerPage;
+    const indexOfFirst = indexOfLast - postPerPage;
+    const currentPosts = (posts) => {
+        let currentPosts = 0;
+        currentPosts = feed.slice(indexOfFirst, indexOfLast);
+        return currentPosts;
+    };
 
     return (
         <div className="home">
@@ -50,7 +65,7 @@ function Feed(props) {
                         </h2>
                     </div>
 
-                    <FeedList dataList={feed}  />
+                    <FeedList dataList={currentPosts(feed)} loading={loading}  />
 
                 </div>
                 <SideMenu></SideMenu>
