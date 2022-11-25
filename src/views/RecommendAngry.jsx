46e8eb5b30214c5f19983151445334b753b74aca
kimdoha <kkhhjj3393@gmail.com>
2022-11-25 15:17:38 +0900

    feat: 감정 별 영화 추천 -> 컴포넌트 변경
    -

--- src/views/RecommendAngry.jsx
+++ src/views/RecommendAngry.jsx
@@ -45,72 +45,9 @@ function RecommendAngry(props) {
                         </h2>
                     </div>
 
-
-                    {/*<section className="movies">*/}
-                    {/*    <div className="movie">*/}
-                    {/*        <img src="https://github.com/supahfunk/supah-codepen/blob/master/movie-room.jpg?raw=true"*/}
-                    {/*             alt="" className="poster"/>*/}
-                    {/*        <div className="title">Room</div>*/}
-                    {/*        <div className="info">*/}
-                    {/*            <span className="length">117 min</span>*/}
-                    {/*            <span className="year">2015</span>*/}
-                    {/*        </div>*/}
-                    {/*        <div className="desc">*/}
-                    {/*            Jack is a young boy of 5 years old who has lived all his life in one room. He believes*/}
-                    {/*            everything within it are the only real things in the world. But what will happen when*/}
-                    {/*            his Ma suddenly tells him that there are other things outside of Room?*/}
-                    {/*        </div>*/}
-                    {/*    </div>*/}
-                    {/*    <div className="movie">*/}
-                    {/*        <img*/}
-                    {/*            src="https://github.com/supahfunk/supah-codepen/blob/master/movie-whiplash.jpg?raw=true"*/}
-                    {/*            alt="" className="poster"/>*/}
-                    {/*        <div className="title">Whiplash</div>*/}
-                    {/*        <div className="info">*/}
-                    {/*            <span className="length">167 min</span>*/}
-                    {/*            <span className="year">2015</span>*/}
-                    {/*        </div>*/}
-                    {/*        <div className="desc">*/}
-                    {/*            Under the direction of a ruthless instructor, a talented young drummer begins to pursue*/}
-                    {/*            perfection at any cost, even his humanity.*/}
-                    {/*        </div>*/}
-                    {/*    </div>*/}
-                    {/*    <div className="movie">*/}
-                    {/*        <img src="https://github.com/supahfunk/supah-codepen/blob/master/movie-madmax.jpg?raw=true"*/}
-                    {/*             alt="" className="poster"/>*/}
-                    {/*        <div className="title">Mad Max</div>*/}
-                    {/*        <div className="info">*/}
-                    {/*            <span className="length">120 min</span>*/}
-                    {/*            <span className="year">2015</span>*/}
-                    {/*        </div>*/}
-                    {/*        <div className="desc">*/}
-                    {/*            An apocalyptic story set in the furthest reaches of our planet, in a stark desert*/}
-                    {/*            landscape where humanity is broken, and most everyone is crazed fighting for the*/}
-                    {/*            necessities of life. Within this world exist two rebels on the run who just might be*/}
-                    {/*            able to restore order. There's Max, a man of action and a man of few words, who seeks*/}
-                    {/*            peace of mind following the loss of his wife and child in the aftermath of the chaos.*/}
-                    {/*        </div>*/}
-                    {/*    </div>*/}
-                    {/*    <div className="movie">*/}
-                    {/*        <img*/}
-                    {/*            src="https://github.com/supahfunk/supah-codepen/blob/master/movie-therevenant.jpg?raw=true"*/}
-                    {/*            alt="" className="poster"/>*/}
-                    {/*        <div className="title">The Revenant</div>*/}
-                    {/*        <div className="info">*/}
-                    {/*            <span className="length">156 min</span>*/}
-                    {/*            <span className="year">2015</span>*/}
-                    {/*        </div>*/}
-                    {/*        <div className="desc">*/}
-                    {/*            In the 1820s, a frontiersman, Hugh Glass, sets out on a path of vengeance against those*/}
-                    {/*            who left him for dead after a bear mauling.*/}
-                    {/*        </div>*/}
-                    {/*    </div>*/}
-                    {/*</section>*/}
-
                     <div className="movies">
                         { results.map(movie => (
                             <Movie
-                                // 유일해야하므로 id 를 줘야한다.
                                 key={movie.id}
                                 id={movie.id}
                                 year={movie.release_date}
